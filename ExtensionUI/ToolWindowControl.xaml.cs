065af70cfe00a287795f63e9fd8f966dadd5f248
daniel.kovacs314@gmail.com <DDoSinho>
2018-11-19 23:18:37 +0100

    FolderPicker implementation

--- ExtensionUI/ToolWindowControl.xaml.cs
+++ ExtensionUI/ToolWindowControl.xaml.cs
@@ -1,5 +1,6 @@
 ﻿namespace ExtensionUI
 {
+    using ExtensionUI.ViewModels;
     using System.Diagnostics.CodeAnalysis;
     using System.Windows;
     using System.Windows.Controls;
@@ -9,13 +10,28 @@
     /// </summary>
     public partial class ToolWindowControl : UserControl
     {
+        public FileViewModel FileViewModel { get; set; } = new FileViewModel();
+
         /// <summary>
         /// Initializes a new instance of the <see cref="ToolWindowControl"/> class.
         /// </summary>
         public ToolWindowControl()
         {
             this.InitializeComponent();
+            this.DataContext = FileViewModel;
         }
-        
+
+        private void btn_browse_Click(object sender, RoutedEventArgs e)
+        {
+            using(var dialog = new System.Windows.Forms.FolderBrowserDialog())
+            {
+                var dialogResult = dialog.ShowDialog();
+
+                if(dialogResult == System.Windows.Forms.DialogResult.OK)
+                {
+                    FileViewModel.PickedFolderPath = dialog.SelectedPath;
+                }
+            }
+        }
     }
 }
---
+++ ExtensionUI/ViewModels/FileViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionUI.ViewModels
+{
+    public class FileViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _pickedFolderPath;
+        public string PickedFolderPath
+        {
+            get
+            {
+                return _pickedFolderPath;
+            }
+            set
+            {
+                _pickedFolderPath = value;
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PickedFolderPath)));
+            }
+        }
+
+
+    }
+}
