02dbf21484b5f66c7d03d33bbc8dd28d80ae8d24
daniel.kovacs314@gmail.com <DDoSinho>
2018-11-21 10:55:02 +0100

    UI kialakítása

--- ExtensionUI/ToolWindowControl.xaml.cs
+++ ExtensionUI/ToolWindowControl.xaml.cs
@@ -10,15 +10,28 @@
     /// </summary>
     public partial class ToolWindowControl : UserControl
     {
-        public FileViewModel FileViewModel { get; set; } = new FileViewModel();
+        public ToolViewModel ToolViewModel { get; set; } = new ToolViewModel();
 
         /// <summary>
         /// Initializes a new instance of the <see cref="ToolWindowControl"/> class.
         /// </summary>
         public ToolWindowControl()
         {
+            ToolViewModel.ItemList = new System.Collections.ObjectModel.ObservableCollection<string>();
+            ToolViewModel.ItemList.Add("alma1");
+            ToolViewModel.ItemList.Add("alma2");
+            ToolViewModel.ItemList.Add("alma3");
+            ToolViewModel.ItemList.Add("almafa");
+            ToolViewModel.ItemList.Add("barack1");
+            ToolViewModel.ItemList.Add("barack2");
+            ToolViewModel.ItemList.Add("barack3");
+            ToolViewModel.ItemList.Add("barack4");
+            ToolViewModel.ItemList.Add("barackfa");
+
             this.InitializeComponent();
-            this.DataContext = FileViewModel;
+
+            ToolViewModel.PickedFolderPath = "Select folder";
+            this.DataContext = ToolViewModel;
         }
 
         private void btn_browse_Click(object sender, RoutedEventArgs e)
@@ -29,7 +42,7 @@
 
                 if(dialogResult == System.Windows.Forms.DialogResult.OK)
                 {
-                    FileViewModel.PickedFolderPath = dialog.SelectedPath;
+                    ToolViewModel.PickedFolderPath = dialog.SelectedPath;
                 }
             }
         }
---
+++ ExtensionUI/ViewModels/ToolViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionUI.ViewModels
+{
+    public class ToolViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _pickedFolderPath;
+        public string PickedFolderPath
+        {
+            get =>  _pickedFolderPath;
+            set
+            {
+                _pickedFolderPath = value;
+
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(PickedFolderPath)));
+            }
+        }
+
+        private string _searchText;
+
+        public string SearchText
+        {
+            get => _searchText;
+            set
+            {
+                if (_searchText == value)
+                    return;
+
+                _searchText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(SearchText)));
+            }
+        }
+
+        public ObservableCollection<string> ItemList { get; set; }
+    }
+}
