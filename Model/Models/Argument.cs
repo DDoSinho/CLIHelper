82e899783f900417910b72adfa5deaca240a22e9
KalBence <kalman.bence95@gmail.com>
2018-11-23 21:10:05 +0100

    Added model

---
+++ Model/Models/Argument.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Models
+{
+	public class Argument : CmdBase
+	{
+		//public string Value { get; set; }
+
+		public int NumberOfParams { get; set; }
+
+		//public string SecondValue { get; set; }
+	}
+}
---
+++ Model/Models/Command.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Models
+{
+	public class Command : CmdBase
+	{
+		public List<Option> Options { get; set; }
+
+		public List<Argument> Arguments { get; set; }
+	}
+}
