146724b5b0ae5445a74896d4d82b7783ffe8cda1
KalBence <kalman.bence95@gmail.com>
2018-11-26 16:02:39 +0100

    Model ext

---
+++ Model/Models/CLITool.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Models
+{
+	public class CLITool
+	{
+		public string Name { get; set; }
+
+		public string ExecutableFile { get; set; }
+
+		public List<Command> Commands { get; set; }
+	}
+}
